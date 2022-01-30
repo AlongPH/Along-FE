8b08f83bc3941e6c8968c107435d5adb80356d02
zerocy18 <zerocy18@gmail.com>
2022-01-30 23:02:07 +0900

    ref #220126 회원form

--- components/LabelInput/LabelInput.tsx
+++ components/LabelInput/LabelInput.tsx
@@ -3,12 +3,17 @@ import { Container, LabelContainer, InputContainer } from './style'
 import LabelInputProps from './interface'
 
 const LabelInput = (props: LabelInputProps) => {
-  const { text, value, type = 'text', onChange } = props
+  const { name, text, value, type = 'text', onChange } = props
 
   return (
     <Container>
       <LabelContainer>{text}</LabelContainer>
-      <InputContainer value={value} onChange={onChange} type={type} />
+      <InputContainer
+        name={name}
+        value={value}
+        onChange={onChange}
+        type={type}
+      />
     </Container>
   )
 }
